fc171b67d50787bf2d3c1205fac6fffaa53b52a8
Jonah Lotegeluaki <25lotegj@elmbrookstudents.org>
2020-12-16 16:47:13 +0000

    Add "Other Side"

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function showStories() {
     document.getElementById("stories").style.display = "block";
+    document.getElementById("stories").style.color = "white";
 }
 function hideStories() {
     document.getElementById("stories").style.display = "none";
+    document.getElementById("stories").style.color = "white";
 }
 function toggleStories() {
     if (document.getElementById("stories").style.display == "none") {
